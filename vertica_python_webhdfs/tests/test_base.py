923da04430d4410d4159d0610811f6688f4f592f
Ádám Lippai <adam@rigo.sk>
2024-01-04 23:31:42 -0500

    Add server impl

--- vertica_python_webhdfs/tests/test_base.py
+++ vertica_python_webhdfs/tests/test_base.py
@@ -3,25 +3,45 @@ from multiprocessing import Process
 import pytest
 import requests
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
 import vertica_python as vp
+import json
 
 app = FastAPI
 
-@app.get('{full_path:path}')
+@app.get('/{full_path:path}')
 def all_get(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
+    JSONResponse(
+        status_code=404,
+        content={
+            "RemoteException":
+            {
+                "exception"    : "FileNotFoundException",
+                "javaClassName": "java.io.FileNotFoundException",
+                "message"      : "File does not exist: /foo/a.patch"
+            }
+        }
+    )
+    
 
-@app.put('{full_path:path}')
-def all_put(request: Request, full_path: str):
+@app.put('/{full_path:path}')
+async def all_put(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
+    if request.query_param['op'] == 'MKDIRS':
+        return {'boolean': True}
+    if request.query_param['op'] == 'CREATE':
+        await request.body()
+        return Response(status_code=201)
 
-@app.delete('{full_path:path}')
+@app.delete('/{full_path:path}')
 def all_delete(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
+    return {'boolean': True}
 
 def run_server():
     uvicorn.run(app)
