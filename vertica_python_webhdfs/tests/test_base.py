ca0a78ef8918fc2a08d9fb3034aead2e803ae9e6
Ádám Lippai <adam@rigo.sk>
2024-01-04 23:50:13 -0500

    Try exposing host port

--- vertica_python_webhdfs/tests/test_base.py
+++ vertica_python_webhdfs/tests/test_base.py
@@ -10,56 +10,64 @@ import json
 
 app = FastAPI()
 
-@app.get('/{full_path:path}')
+
+@app.get("/{full_path:path}")
 def all_get(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
     JSONResponse(
         status_code=404,
         content={
-            "RemoteException":
-            {
-                "exception"    : "FileNotFoundException",
+            "RemoteException": {
+                "exception": "FileNotFoundException",
                 "javaClassName": "java.io.FileNotFoundException",
-                "message"      : "File does not exist: /foo/a.patch"
+                "message": "File does not exist: /foo/a.patch",
             }
-        }
+        },
     )
-    
 
-@app.put('/{full_path:path}')
+
+@app.put("/{full_path:path}")
 async def all_put(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
-    if request.query_param['op'] == 'MKDIRS':
-        return {'boolean': True}
-    if request.query_param['op'] == 'CREATE':
+    if request.query_param["op"] == "MKDIRS":
+        return {"boolean": True}
+    if request.query_param["op"] == "CREATE":
         await request.body()
         return Response(status_code=201)
 
-@app.delete('/{full_path:path}')
+
+@app.delete("/{full_path:path}")
 def all_delete(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_param}")
-    return {'boolean': True}
+    return {"boolean": True}
+
 
 def run_server():
     uvicorn.run(app)
+    print('server running')
 
 
 @pytest.fixture
 def server():
     proc = Process(target=run_server, args=(), daemon=True)
-    proc.start() 
+    proc.start()
     yield
-    proc.kill() # Cleanup after test
+    proc.kill()  # Cleanup after test
 
 
 def test_read_main(server):
-    connection = vp.connect(**{'host': '127.0.0.1',
-             'port': 5433,
-             'user': 'dbadmin',
-             'database': 'VMart'
-             })
-    print(connection.cursor().execute('select 1').fetchall())
-    print(connection.cursor().execute("EXPORT TO PARQUET(directory = 'webhdfs://127.0.0.1:8000/virtualdata') AS SELECT 1 AS account_id").fetchall())
+    connection = vp.connect(
+        **{"host": "127.0.0.1", "port": 5433, "user": "dbadmin", "database": "VMart"}
+    )
+    print(connection.cursor().execute("select 1").fetchall())
+    print('trying to connect server')
+    print(
+        connection.cursor()
+        .execute(
+            "EXPORT TO PARQUET(directory = 'webhdfs://127.0.0.1:8000/virtualdata') AS SELECT 1 AS account_id"
+        )
+        .fetchall()
+    )
