1121a717397fc1ce94f3b7b9a712a5e4916c8cfd
Ádám Lippai <adam@rigo.sk>
2024-01-06 15:45:32 -0500

    Add more ops

--- vertica_python_webhdfs/tests/test_base.py
+++ vertica_python_webhdfs/tests/test_base.py
@@ -4,46 +4,67 @@ import pytest
 import requests
 import uvicorn
 from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse
 import vertica_python as vp
-import json
+from io import BytesIO
+import pyarrow.parquet as pq
+
+GENERIC_NOT_FOUND = f"""{{
+    "RemoteException": {{
+        "exception": "FileNotFoundException",
+        "javaClassName": "java.io.FileNotFoundException",
+        "message": "File does not exist: /foo/a.patch"
+    }}
+}}"""
+
+def generic_file(size: int) -> str:
+    return f'''{{
+    "FileStatus": {{
+        "accessTime"      : 0,
+        "blockSize"       : 0,
+        "group"           : "",
+        "length"          : {size},
+        "modificationTime": 0,
+        "owner"           : "",
+        "pathSuffix"      : "",
+        "permission"      : "666",
+        "replication"     : 1,
+        "type"            : "FILE"
+    }}
+}}'''
 
 app = FastAPI()
+app.files = {}
 
-
-@app.get("/{full_path:path}")
-def all_get(request: Request, full_path: str):
+@app.route("/{full_path:path}")
+async def all(request: Request, full_path: str):
     print(f"full path {full_path}")
     print(f"query param {request.query_params}")
-    return JSONResponse(
-        status_code=404,
-        content={
-            "RemoteException": {
-                "exception": "FileNotFoundException",
-                "javaClassName": "java.io.FileNotFoundException",
-                "message": "File does not exist: /foo/a.patch",
-            }
-        },
-    )
-
-
-@app.put("/{full_path:path}")
-async def all_put(request: Request, full_path: str):
-    print(f"full path {full_path}")
-    print(f"query param {request.query_params}")
-    if request.query_params["op"] == "MKDIRS":
-        return {"boolean": True}
+    if request.query_params['op'] == "GETFILESTATUS":
+        if full_path in app.files:
+            return Response(generic_file(app.files[full_path]), 200, media_type='application/json')
+        else:
+            return Response(GENERIC_NOT_FOUND, 404, media_type='application/json')
+    if request.query_params['op'] == "MKDIRS":
+        return Response('{"boolean": true}', 200, media_type='application/json')
     if request.query_params["op"] == "CREATE":
-        await request.body()
-        return Response(status_code=201)
-
-
-@app.delete("/{full_path:path}")
-def all_delete(request: Request, full_path: str):
-    print(f"full path {full_path}")
-    print(f"query param {request.query_params}")
-    return {"boolean": True}
-
+        if "create_redirected" not in request.query_params:
+            return Response(status_code=307, headers={"location": f"http://{request.base_url.host}:{request.base_url.port}/{full_path}?{request.query_params}&create_redirected=true"})
+        else:
+            # consume the body and asserts it's empty
+            assert await request.body() == 0
+            app.files[full_path] = 0
+            return Response(status_code=201, headers={"location": f"hdfs://{request.base_url.host}:{request.base_url.port}/{full_path}"})
+    if request.query_params["op"] in {"DELETE", "TRUNCATE"}:
+        return Response('{"boolean": true}', 200, media_type='application/json')
+    if request.query_params["op"] == "APPEND":
+        if "append_redirected" not in request.query_params:
+            return Response(status_code=307, headers={"location": f"http://{request.base_url.host}:{request.base_url.port}/{full_path}?{request.query_params}&append_redirected=true"})
+        else:
+            ret = await request.body()
+            b = BytesIO(ret)
+            app.files[full_path] += len(ret)
+            results.append(pq.read_table(b))
+            return Response(status_code=200)
 
 def run_server():
     uvicorn.run(app)
