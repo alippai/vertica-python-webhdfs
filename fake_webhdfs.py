81064959ea47323299731fbfda79bfe40f534f08
Ádám Lippai <adam@rigo.sk>
2024-01-06 16:34:00 -0500

    Change global vars scope

--- fake_webhdfs.py
+++ fake_webhdfs.py
@@ -36,8 +36,8 @@ async def get_handler(request: Request, full_path: str) -> Response:
     print(f"full path {full_path}")
     print(f"query param {request.query_params}")
     if request.query_params['op'] == "GETFILESTATUS":
-        if full_path in app.files:
-            return Response(generic_file(app.files[full_path]), 200, media_type='application/json')
+        if full_path in files:
+            return Response(generic_file(files[full_path]), 200, media_type='application/json')
         else:
             return Response(GENERIC_NOT_FOUND, 404, media_type='application/json')
 
@@ -53,7 +53,7 @@ async def put_handler(request: Request, full_path: str) -> Response:
         else:
             # consume the body and asserts it's empty
             assert await request.body() == 0
-            app.files[full_path] = 0
+            files[full_path] = 0
             return Response(status_code=201, headers={"location": f"hdfs://{request.base_url.host}:{request.base_url.port}/{full_path}"})
     if request.query_params["op"] == "RENAME":
         return Response('{"boolean": true}', 200, media_type='application/json')
@@ -77,6 +77,6 @@ async def post_handler(request: Request, full_path: str) -> Response:
         else:
             ret = await request.body()
             b = BytesIO(ret)
-            app.files[full_path] += len(ret)
+            files[full_path] += len(ret)
             results.append(pq.read_table(b))
             return Response(status_code=200)
