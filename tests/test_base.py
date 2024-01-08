0078c27bfa93051224a12cf31ba0133acc025ee7
Ádám Lippai <adam@rigo.sk>
2024-01-07 19:26:57 -0500

    Debug, remove deps

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,12 +7,15 @@ import vertica_python as vp
 
 from global_state import finish
 
+i = 0
+
 class Server(uvicorn.Server):
     def install_signal_handlers(self):
         pass
 
     @contextlib.contextmanager
     def run_in_thread(self):
+        print('run_in_thread')
         thread = threading.Thread(target=self.run)
         print('Starting')
         thread.start()
@@ -21,14 +24,21 @@ class Server(uvicorn.Server):
             while not self.started:
                 print(f'Not started: {self.started}')
                 time.sleep(1)
+                if i > 30:
+                    raise Exception('Server start failed')
+                else:
+                    i += 1
             yield
         finally:
             print('Exiting')
             self.should_exit = True
             thread.join()
 
+print('Before config')
 config = uvicorn.Config("fake_webhdfs:app", host="127.0.0.1", port=8000, log_level="trace")
+print('Before server')
 server = Server(config=config)
+print('After server')
 
 def test_read_main():
     connection = vp.connect(
